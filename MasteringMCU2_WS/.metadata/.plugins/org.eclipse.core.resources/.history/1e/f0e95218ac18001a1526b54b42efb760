244455ee91dbc7ed41135d58696dd44cd89a0d32
subhendu23 <subhendu.biswal@gmail.com>
2019-12-07 00:25:49 -0500

    Completed clock HSE, HSI and PLL Dec_07

---
+++ MasteringMCU2_WS/.metadata/.plugins/org.eclipse.core.resources/.history/1e/f0e95218ac18001a1526b54b42efb760
@@ -0,0 +1,5 @@
+
+#ifndef MAIN_H_
+#define MAIN_H_
+#include "stm32f4xx_hal.h"
+#endif
---
+++ MasteringMCU2_WS/PLL_SYSCLK/Src/it.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+
+void SysTick_Handler(void)
+{
+
+	HAL_IncTick();
+	HAL_SYSTICK_IRQHandler();
+}
+
